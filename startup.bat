d29d51bd27625402f80d3b5bb92ab761fb0e8bf8
suganm06 <72401487+suganm06@users.noreply.github.com>
2021-02-07 21:00:00 +0530

    Add files via upload

---
+++ startup.bat
@@ -0,0 +1,8 @@
+#to run your program aster windows startup
+#open notepad 
+cd C:\Users\123.DESKTOP-74G99SN\py\programs #type 'cd' and 'path' of your file
+python voice_assistant.py #type the name of your file
+#save the file as .bat in name
+#press 'windows keys' + 'R'
+#type 'shell:startup'
+#copy the saved file and paste it in that folder
